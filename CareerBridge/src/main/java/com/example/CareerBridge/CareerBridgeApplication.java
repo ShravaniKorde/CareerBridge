d3d822fc95c96dd2d1f1a0e80ff80ee190ebd7ea
Shravani <shravani.korde11@gmail.com>
2025-09-16 16:36:09 +0530

    Initial commit

---
+++ CareerBridge/src/main/java/com/example/CareerBridge/CareerBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.CareerBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerBridgeApplication.class, args);
+	}
+
+}
